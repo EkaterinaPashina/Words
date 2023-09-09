b9f4a8b2221008a393e403c629e1ac74ace936dc
EkaterinaPashina <k377at@mail.ru>
2023-09-09 16:56:32 +0500

    added fruits, vegetables, berries, herbs, cereals

---
+++ words/src/components/words.js
@@ -0,0 +1,617 @@
+const fruitsAndVegetables = [
+
+// Общее
+    {"english": "fruit",
+    "russian": "фрукт",
+    "transcription": "[fru:t]",
+    "topic": "fruits"},
+
+    {"english": "vegetable",
+    "russian": "овощ",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "berry",
+    "russian": "ягода",
+    "transcription": "",
+    "topic": "berries"},
+
+// Фрукты
+    {"english": "kiwi",
+    "russian": "киви",
+    "transcription": "[]",
+    "topic": "fruits"},
+    
+    {"english": "papaya",
+    "russian": "папайя",
+    "transcription": "[]",
+    "topic": "fruits"},
+
+    {"english": "coconut",
+    "russian": "кокос",
+    "transcription": "[]",
+    "topic": "fruits"},
+
+    {"english": "prune",
+    "russian": "чернослив",
+    "transcription": "[pru:n]",
+    "topic": "fruits"},
+
+    {"english": "quince",
+    "russian": "айва",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "date",
+    "russian": "финик",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "watermelon",
+    "russian": "арбуз",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "banana",
+    "russian": "банан",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "orange",
+    "russian": "апельсин",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "pip",
+    "russian": "косточка",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "pineapple",
+    "russian": "ананас",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "peach",
+    "russian": "персик",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "passion fruit",
+    "russian": "маракуйя",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "apple",
+    "russian": "яблоко",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "melon",
+    "russian": "дыня",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "lemon",
+    "russian": "лимон",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "pear",
+    "russian": "груша",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "apricot",
+    "russian": "абрикос",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "mango",
+    "russian": "манго",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "pomegranate",
+    "russian": "гранат",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "citrus fruit",
+    "russian": "цитрусовые",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "grape",
+    "russian": "виноград",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "plum",
+    "russian": "слива",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "nectarine",
+    "russian": "нектарин",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "fig",
+    "russian": "инжир",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "lime",
+    "russian": "лайм",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "tangerine",
+    "russian": "мандарин",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "grapefruit",
+    "russian": "грейпфрут",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "persimmon",
+    "russian": "хурма",
+    "transcription": "",
+    "topic": "fruits"},
+
+    {"english": "kumquat",
+    "russian": "кумкват",
+    "transcription": "",
+    "topic": "fruits"},
+
+// Ягоды
+    {"english": "raspberry",
+    "russian": "малина",
+    "transcription": "['ra:zbəri]",
+    "topic": "berries"},
+
+    {"english": "strawberry",
+    "russian": "клубника",
+    "transcription": "['strɔ:bəri]",
+    "topic": "berries"},
+
+    {"english": "cranberry",
+    "russian": "клюква",
+    "transcription": "['krænbəri]",
+    "topic": "berries"},
+
+    {"english": "blackberry",
+    "russian": "ежевика",
+    "transcription": "",
+    "topic": "berries"},
+
+    {"english": "redcurrant",
+    "russian": "красная смородина",
+    "transcription": "",
+    "topic": "berries"},
+
+    {"english": "lingonberry",
+    "russian": "брусника",
+    "transcription": "",
+    "topic": "berries"},
+
+    {"english": "blackcurrant",
+    "russian": "черная смородина",
+    "transcription": "",
+    "topic": "berries"},
+
+    {"english": "blueberry",
+    "russian": "черника",
+    "transcription": "",
+    "topic": "berries"},
+
+    {"english": "cherry",
+    "russian": "вишня",
+    "transcription": "",
+    "topic": "berries"},
+
+    {"english": "currant",
+    "russian": "смородина",
+    "transcription": "",
+    "topic": ""},
+
+// Овощи
+    {"english": "garlic",
+    "russian": "чеснок",
+    "transcription": "[ga:lik]",
+    "topic": "vegetables",
+    "subtopic": "bulb vegetables"},
+
+    {"english": "beet",
+    "russian": "свекла",
+    "transcription": "[bi:t]",
+    "topic": "vegetables"},
+
+    {"english": "eggplant",
+    "russian": "баклажан",
+    "transcription": "[]",
+    "topic": "vegetables"},
+
+    {"english": "radish",
+    "russian": "редиска",
+    "transcription": "[]",
+    "topic": "vegetables"},
+
+    {"english": "tomato",
+    "russian": "помидор",
+    "transcription": "[]",
+    "topic": "vegetables"},
+
+    {"english": "cauliflower",
+    "russian": "цветная капуста",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "brussel sprout",
+    "russian": "брюссельская капуста",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "green onion",
+    "russian": "зеленый лук",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "sorrel",
+    "russian": "щавель",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "ginger",
+    "russian": "имбирь",
+    "transcription": "",
+    "topic": "vegetables",
+    "subtopic": "root"},
+
+    {"english": "rhubarb",
+    "russian": "ревень",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "paprika",
+    "russian": "перец паприка",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "rutabaga",
+    "russian": "брюква",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "onion",
+    "russian": "репчатый лук",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "corn",
+    "russian": "кукуруза",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "celery",
+    "russian": "сельдерей",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "avocado",
+    "russian": "авокадо",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "spinach",
+    "russian": "шпинат",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "artichoke",
+    "russian": "артишок",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "turnip",
+    "russian": "репа",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "chilli",
+    "russian": "перец чили",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "courgette",
+    "russian": "кабачок",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "arugula",
+    "russian": "руккола",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "greens",
+    "russian": "зелень",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "sweet potato",
+    "russian": "сладкий картофель",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "broccoli",
+    "russian": "брокколи",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "cabbage",
+    "russian": "капуста",
+    "transcription": "",
+    "topic": "vegetables"},
+
+    {"english": "potato",
+    "russian": "картошка",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "asparagus",
+    "russian": "спаржа",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "red pepper",
+    "russian": "красный перец",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "parsnip",
+    "russian": "пастернак",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "marrow",
+    "russian": "кабачок",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "jerusalem artichoke",
+    "russian": "топинамбур",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "chinese cabbage",
+    "russian": "китайская капуста",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "kohlrabi",
+    "russian": "кольраби",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "zucchini",
+    "russian": "цуккини",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "pumpkin",
+    "russian": "тыква",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "carrot",
+    "russian": "морковь",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "pea",
+    "russian": "горох",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "cucumber",
+    "russian": "огурец",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "lettuce",
+    "russian": "салат-латук",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "kidney bean",
+    "russian": "фасоль",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "head",
+    "russian": "кочан (капусты)",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "cherry tomato",
+    "russian": "помидоры черри",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "horseradish",
+    "russian": "хрен",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "leek",
+    "russian": "лук-порей",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "daikon",
+    "russian": "дайкон",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "maize",
+    "russian": "кукуруза, маис",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "champignon",
+    "russian": "шампиньон",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "olive",
+    "russian": "оливка",
+    "transcription": "",
+    "topic": "vegetables"},
+    
+    {"english": "nuts",
+    "russian": "орехи",
+    "transcription": "",
+    "topic": "nuts"},
+    
+    {"english": "lychee",
+    "russian": "личи",
+    "transcription": "",
+    "topic": "fruits"},
+    
+    {"english": "feijoa",
+    "russian": "фейхоа",
+    "transcription": "",
+    "topic": "fruits"},
+
+//травы
+    {"english": "dill",
+    "russian": "укроп",
+    "transcription": "",
+    "topic": "herbs"},
+
+    {"english": "anise",
+    "russian": "анис",
+    "transcription": "",
+    "topic": ""},
+    
+    {"english": "bay",
+    "russian": "лавровый лист",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "oregano",
+    "russian": "орегано",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "tarragon",
+    "russian": "эстрагон",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "basil",
+    "russian": "базилик",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "sage",
+    "russian": "шалфей",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "thyme",
+    "russian": "чабрец",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "mint",
+    "russian": "мята",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "parsley",
+    "russian": "петрушка",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "coriander",
+    "russian": "кориандр",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "rosemary",
+    "russian": "розмарин",
+    "transcription": "",
+    "topic": ""},
+//крупы
+    {"english": "rice",
+    "russian": "рис",
+    "transcription": "",
+    "topic": "cereals"},
+
+    {"english": "spelt wheat",
+    "russian": "полба",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "wheat",
+    "russian": "пшеница",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "oats",
+    "russian": "овес",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "rye",
+    "russian": "рожь",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "millet",
+    "russian": "пшено",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "corn",
+    "russian": "кукуруза",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "barley",
+    "russian": "ячмень",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "buckwheat",
+    "russian": "гречка",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "quinoa",
+    "russian": "киноа",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "",
+    "russian": "",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "",
+    "russian": "",
+    "transcription": "",
+    "topic": ""},
+
+    {"english": "",
+    "russian": "",
+    "transcription": "",
+    "topic": ""},
+
+];
+
+export default fruitsAndVegetables;
